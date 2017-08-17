f38a38e9d3931a434bab77620f892a682150347f
jinxl <121552497@qq.com>
2017-08-17 11:33:48 +0800

    改变结构

--- app/src/main/java/com/kkk/jinxl/androidpractices/activity/MainActivity.java
+++ app/src/main/java/com/kkk/jinxl/androidpractices/activity/MainActivity.java
@@ -1,8 +1,9 @@
-package com.kkk.jinxl.androidpractices;
+package com.kkk.jinxl.androidpractices.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
+import com.kkk.jinxl.androidpractices.R;
 
 public class MainActivity extends AppCompatActivity {
 
